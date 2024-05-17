35dfb26d1fb0fda26bcb043e2c7963141e235975
Makotache <victorcullet@gmail.com>
2024-05-17 12:37:33 +0200

    correctif toggleActiveMenu

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,5 +1,7 @@
 const toggleActiveMenu = (menuItems, url) =>
 {
+    if (menuItems.find(menu => menu.path == url) == undefined)
+    { return; }
     const indexMenu = menuItems.indexOf(menuItems.find(menu => menu.path == url));
     menuItems[indexMenu].isActive = true;
     menuItems.filter(menu => menu.path != url).forEach(menu => menu.isActive = false);
